b567d3c8cff5ae77221bd085813847b403311309
a05h <a117q35mt@gmail.com>
2017-04-22 23:44:34 -0400

    install script upd

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+if [ $USER != 'root' ]; then
+  echo 'You should run it as root.'
+else
+  mkdir ~/.grub
+  cp /build ~/.grub
+  sed -i 's/^GRUB_THEME=".*"$/GRUB_THEME="/home/$USER/.grub/grub-minimal/theme.txt"/g' /etc/default/grub
+  update grub
+fi
