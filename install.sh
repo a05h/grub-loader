45936ce4baf94fa3c40a467009b880fe03c32766
a05h <a117q35mt@gmail.com>
2017-04-25 22:33:28 -0400

    install script: path fix

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ if [ $USER != 'root' ]; then
 else
   mkdir ~/.grub
   cp /build ~/.grub
-  sed -i 's/^GRUB_THEME=".*"$/GRUB_THEME="/home/$USER/.grub/grub-minimal/theme.txt"/g' /etc/default/grub
+  sed -i 's/^GRUB_THEME=".*"$/GRUB_THEME="\/home\/'$SUDO_USER'\/.grub\/grub-minimal\/theme.txt"/g' build/test
   update grub
 fi
